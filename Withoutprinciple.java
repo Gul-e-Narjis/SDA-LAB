430a4ce283b5dec56976422862c679116fcb9e05
Gul-e-Narjis <gulenargis520@gmail.com>
2025-03-09 21:36:13 +0500

    changes in orderclass

---
+++ Withoutprinciple.java
@@ -0,0 +1,11 @@
+class order{
+    private String orderId;
+    private String FoodName;
+     public order(String orderId, String FoodName ){
+        this.orderId=orderId;
+        this.FoodName=FoodName;
+     }
+}
+public class Withoutprinciple {
+    
+}
