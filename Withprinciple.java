267bb946fff4e81a285d496549729fe5b24b3b4c
Gul-e-Narjis <gulenargis520@gmail.com>
2025-03-09 21:27:07 +0500

    changes in withprinciple

--- Withprinciple.java
+++ Withprinciple.java
@@ -9,3 +9,24 @@ class Food {
         return name;
     }
 }
+class Order {
+    private String orderId;
+    private Food food; // Order "owns" Food
+
+    public Order(String orderId, String foodName) {
+        this.orderId = orderId;
+        this.food = new Food(foodName); // Creator Principle Used
+    }
+
+    public void showOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Food Ordered: " + food.getName());
+    }
+}
+
+public class Withprinciple {
+    public static void main(String[] args) {
+        Order order1 = new Order("152", "Pizza");
+        order1.showOrderDetails();
+    }
+}
