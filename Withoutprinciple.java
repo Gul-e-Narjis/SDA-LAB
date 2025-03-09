c940c7c506c6e5a87597dc30efc72235e43f3fa1
Gul-e-Narjis <gulenargis520@gmail.com>
2025-03-09 21:46:39 +0500

    changes in withoutprinciple class

--- Withoutprinciple.java
+++ Withoutprinciple.java
@@ -5,7 +5,25 @@ class order{
         this.orderId=orderId;
         this.FoodName=FoodName;
      }
+     public void showOrderDetails(){
+        System.out.println("Order ID " + orderId);
+        System.out.println("Food Ordered "+ FoodName);
+     }
+}
+class Food{
+    private String name;
+    
+    public Food(String name){
+        this.name=name;
+    }
+    public String getname(){
+        return name;
+    }
 }
 public class Withoutprinciple {
-    
+    public static void main(String[] args) {
+        Order order1=new Order("152", "Pizza");
+        order1.showOrderDetails();
+
+    }
 }
